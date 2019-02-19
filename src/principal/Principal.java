d464dfa17f96d6202011c1d2c8cafd8ec09c4084
nexoZero <sigma.leonnard@gmail.com>
2019-02-20 02:41:37 +0700

    Projeto criado

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("INICIANDO APLICACAO COM BANCO EMBARCADO");
+    }
+    
+}
